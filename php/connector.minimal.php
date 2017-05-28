40e04c23ee15ed5c8e00019d658b1ebec645910f
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-28 19:39:09 +0700

    PHP CS Fixer

--- php/connector.minimal.php
+++ php/connector.minimal.php
@@ -81,45 +81,45 @@ elFinder::$netDrivers['ftp'] = 'FTP';
  * @param  string    $relpath file path relative to volume root directory started with directory separator
  * @return bool|null
  **/
-function access($attr, $path, $data, $volume, $isDir, $relpath) {
-	return basename($path)[0] === '.'            // if file/folder begins with '.' (dot) with out volume root
-			 && strlen($relpath) !== 1
-		? !($attr == 'read' || $attr == 'write') // set read+write to false, other (locked+hidden) set to true
-		:  null;                                 // else elFinder decide it itself
+function access($attr, $path, $data, $volume, $isDir, $relpath)
+{
+    return basename($path)[0] === '.'            // if file/folder begins with '.' (dot) with out volume root
+             && strlen($relpath) !== 1
+        ? !($attr == 'read' || $attr == 'write') // set read+write to false, other (locked+hidden) set to true
+        :  null;                                 // else elFinder decide it itself
 }
 
 
 // Documentation for connector options:
 // https://github.com/Studio-42/elFinder/wiki/Connector-configuration-options
 $opts = array(
-	// 'debug' => true,
-	'roots' => array(
-		// Items volume
-		array(
-			'driver'        => 'LocalFileSystem',           // driver for accessing file system (REQUIRED)
-			'path'          => '../files/',                 // path to files (REQUIRED)
-			'URL'           => dirname($_SERVER['PHP_SELF']) . '/../files/', // URL to files (REQUIRED)
-			'trashHash'     => 't1_Lw',                     // elFinder's hash of trash folder
-			'uploadDeny'    => array('all'),                // All Mimetypes not allowed to upload
-			'uploadAllow'   => array('image', 'text/plain'),// Mimetype `image` and `text/plain` allowed to upload
-			'uploadOrder'   => array('deny', 'allow'),      // allowed Mimetype `image` and `text/plain` only
-			'accessControl' => 'access'                     // disable and hide dot starting files (OPTIONAL)
-		),
-		// Trash volume
-		array(
-			'id'            => '1',
-			'driver'        => 'Trash',
-			'path'          => '../files/.trash/',
-			'tmbURL'        => dirname($_SERVER['PHP_SELF']) . '/../files/.trash/.tmb/',
-			'uploadDeny'    => array('all'),                // Recomend the same settings as the original volume that uses the trash
-			'uploadAllow'   => array('image', 'text/plain'),// Same as above
-			'uploadOrder'   => array('deny', 'allow'),      // Same as above
-			'accessControl' => 'access',                    // Same as above
-		)
-	)
+    // 'debug' => true,
+    'roots' => array(
+        // Items volume
+        array(
+            'driver'        => 'LocalFileSystem',           // driver for accessing file system (REQUIRED)
+            'path'          => '../files/',                 // path to files (REQUIRED)
+            'URL'           => dirname($_SERVER['PHP_SELF']) . '/../files/', // URL to files (REQUIRED)
+            'trashHash'     => 't1_Lw',                     // elFinder's hash of trash folder
+            'uploadDeny'    => array(),                // All Mimetypes not allowed to upload
+            'uploadAllow'   => array(),// Mimetype `image` and `text/plain` allowed to upload
+            'uploadOrder'   => array(),      // allowed Mimetype `image` and `text/plain` only
+            'accessControl' => null                     // disable and hide dot starting files (OPTIONAL)
+        ),
+        // Trash volume
+        array(
+            'id'            => '1',
+            'driver'        => 'Trash',
+            'path'          => '../files/.trash/',
+            'tmbURL'        => dirname($_SERVER['PHP_SELF']) . '/../files/.trash/.tmb/',
+            'uploadDeny'    => array('all'),                // Recomend the same settings as the original volume that uses the trash
+            'uploadAllow'   => array('image', 'text/plain'),// Same as above
+            'uploadOrder'   => array('deny', 'allow'),      // Same as above
+            'accessControl' => 'access',                    // Same as above
+        )
+    )
 );
 
 // run elFinder
 $connector = new elFinderConnector(new elFinder($opts));
 $connector->run();
-
